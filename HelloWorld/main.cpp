5640fac666c03b402f331bb561678663fe6cd5bb
satyam <satyam0181@gmail.com>
2020-02-19 21:46:23 +0530

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Satyam Kumar on 19/02/20.
+//  Copyright © 2020 Satyam Kumar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
