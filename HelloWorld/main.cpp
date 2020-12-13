21809fdf2f998bea7c94c430ab971fc2b18976e9
Pun11sher <satyam0181@gmail.com>
2020-12-13 16:33:46 +0530

    LA LA LA

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -5,11 +5,3 @@
 //  Created by Satyam Kumar on 19/02/20.
 //  Copyright © 2020 Satyam Kumar. All rights reserved.
 //
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
